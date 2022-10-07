31b003dc932e1569c48757537f5313181ff3423b
Marco Tulio <marcotulio.dev@gmail.com>
2022-10-07 15:36:52 -0300

    arquivos do curso

---
+++ fundamentos/primeiro/go.mod
@@ -0,0 +1,3 @@
+module primeiro
+
+go 1.19
---
+++ fundamentos/primeiro/primeiro.go
@@ -0,0 +1,13 @@
+//Executaveis tem que ter package main
+package main
+
+
+
+/*
+Go se organiza em modulos, precisamos importa-los para utiliza-los
+*/
+import "fmt"
+// Funçao principal do programa
+func main(){
+    fmt.Print("Primeiro programa!")
+}
