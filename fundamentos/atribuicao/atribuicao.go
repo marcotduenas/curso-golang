65c47f97d2815a6577945b0b40b69a1ab257b0b1
Marco Tulio <marcotduenas@gmail.com>
2022-10-15 11:41:27 -0300

    Curso Go

--- fundamentos/atribuicao/atribuicao.go
+++ fundamentos/atribuicao/atribuicao.go
@@ -2,18 +2,18 @@ package main
 
 import "fmt"
 
+func main() {
+	var b byte = 3
 
-func main(){
-    var b byte = 3
+	i := 3
+	i += 3
+	i -= 3
+	i /= 2
+	i *= 2
+	i %= 2
 
-    i := 3
-    i += 3
-    i -= 3
-    i /= 2
-    i *= 2
-    i %= 2
+	x, y := 1, 2
+	x, y = y, x
 
-    x, y := 1, 2
-    x, y = y, x
-
+	fmt.Println(b)
 }
