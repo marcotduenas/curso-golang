f14c5414099c9badcb91367eab22cab6130d3add
Marco Tulio <marcotulio.dev@gmail.com>
2022-11-29 18:41:08 -0300

    Curso Go

---
+++ testes/tabela/strings_test.go
@@ -0,0 +1,33 @@
+package strings
+
+import(
+    "strings"
+    "testing"
+)
+
+const msgIndex = "%s (parte: %s) - indices: esperado (%d) <> encontrado (%d)."
+
+func TestIndex(t *testing.T){
+    testes := []struct{
+        texto string
+        parte string
+        esperado int    
+    }{
+        {"Marco é um humano", "Marco", 0},
+        {"", "", 0},
+        {"Opa", "opa", -1},
+        {"Leenardo", "o", 2},
+    }
+
+    for _, teste := range testes{
+        t.Logf("Massa: %v", teste)
+        atual := strings.Index(teste.texto, teste.parte)
+
+
+        if atual != teste.esperado{
+            t.Errorf(msgIndex, teste.texto, teste.parte, teste.esperado, atual)
+        }
+    }
+
+
+}
